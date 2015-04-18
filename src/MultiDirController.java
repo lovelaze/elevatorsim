74ac49a28aa4a3317c4d97d4bdba09baaf8a2ae9
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-04-18 13:36:21 +0200

    Added MultiDirController

---
+++ src/MultiDirController.java
@@ -0,0 +1,12 @@
+
+public class MultiDirController extends GroupControl {
+
+	public Car getBestElevator(Call call) {
+		return null;
+	}
+
+    public void controlElevators(int time, TrafficPattern.CallPattern currentPattern) {
+
+    }
+
+}
